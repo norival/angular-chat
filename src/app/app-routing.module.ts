ec260b2e6d9e2552ae0cbe4041a371977a1b64bc
Xavier Laviron <xavier.laviron@3dathome.fr>
2021-04-08 14:07:36 +0200

    update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: ChatComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
