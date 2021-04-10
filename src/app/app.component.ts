708b029a63378cfc0a2a595a9187160b935ffa86
Xavier Laviron <xavier.laviron@3dathome.fr>
2021-04-10 09:19:30 +0200

    initiate current user on startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './chat/user.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Chat application';
+
+    constructor(
+        private userService: UserService
+    ) { }
+
+    ngOnInit(): void {
+    }
 }
