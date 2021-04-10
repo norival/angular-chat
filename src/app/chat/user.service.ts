4b896c8948b51049c16414296cbfb204d5c3989f
Xavier Laviron <xavier.laviron@3dathome.fr>
2021-04-10 09:19:14 +0200

    adds name and uuid generators

--- src/app/chat/user.service.ts
+++ src/app/chat/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { UuidService } from '../UuidService';
+import { User } from './user';
+import { uniqueNamesGenerator, Config, adjectives, animals } from 'unique-names-generator';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
-    constructor() { }
+    currentUser: User;
+
+    constructor(
+        uuid: UuidService
+    ) {
+        const customConfig: Config = {
+            dictionaries: [adjectives, animals],
+            separator: ' ',
+            length: 2,
+            style: 'capital',
+        };
+
+        this.currentUser = {
+            uuid: uuid.generate(),
+            nickname: uniqueNamesGenerator(customConfig),
+        };
+    }
 }
